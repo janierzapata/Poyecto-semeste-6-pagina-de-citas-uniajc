30a434875134ce0d2ea5e769ecbd0ef3bd571d77
ergwer99 <janiier2000stiiven@gmail.com>
2020-11-22 16:16:50 -0500

    el comeinzo de la conexion a la base de datos y creacion de tablas y paginas en mysql

---
+++ src/java/beans/sessions/UsuarioFacade.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beans.sessions;
+
+import entidades.Usuario;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author crga_
+ */
+@Stateless
+public class UsuarioFacade extends AbstractFacade<Usuario> {
+
+    @PersistenceContext(unitName = "Proyecto-Semestre-6-UNIAJCPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public UsuarioFacade() {
+        super(Usuario.class);
+    }
+    
+}
